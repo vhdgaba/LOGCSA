24cfab6d04e5f164f891c1cc91377e96a6b2d8bf
Lester Manuel <42403865+lnmanuel@users.noreply.github.com>
2018-09-22 00:24:42 +0800

    Update and rename Search.py to search.py

--- search.py
+++ search.py
@@ -1,15 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep 20 18:56:21 2018
-@author: Lester Manuel
-"""
 #  The program searches for a string in an array and return location index if found.  
 
-class Search:    
+class search:    
     def __init__(self):
         self.usernames = []
     def search(username, usernames : []):
         if username in usernames:
             return usernames.index(username)
         else:
-            return None
+            return 'not found'
