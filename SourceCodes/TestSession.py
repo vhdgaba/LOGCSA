460a7730a395e5e51af11cfdce09272492f8f574
Vince Gaba <vincedjgaba@gmail.com>
2018-10-15 05:06:48 +0800

    Finalized sprints

--- SourceCodes/TestSession.py
+++ SourceCodes/TestSession.py
@@ -5,7 +5,6 @@ Created on Thu Oct  4 01:48:21 2018
 @author: Lester
 """
 
-
 #2015103119 - adviser
 #2015102105 - adviser
 #2015100895 - adviser
@@ -19,16 +18,17 @@ from DataAccess.FileHandler import FileHandler
 
 class TestSession(unittest.TestCase):
     session1 = Session()    
-
+    
     ##### --Test login_advisee()-- #####
+    
     def test_login_advisee_no_peeradviser(self):
         TestSession.session1.peeradvisercount = 0
         self.assertEqual(TestSession.session1.login_advisee(2018999999), True)
-
+    
     def test_registeredsn_login_advisee(self):
         TestSession.session1.peeradvisercount = 1
         self.assertEqual(TestSession.session1.login_advisee(2017123123), True)
-
+        
     def test_loggedregisteredsn_login_advisee(self):
         TestSession.session1.peeradvisercount = 1
         self.assertEqual(TestSession.session1.login_advisee(2018999999), True)
@@ -42,20 +42,19 @@ class TestSession(unittest.TestCase):
 
     def test_wsnwpw_login_peeradviser(self):
         self.assertEqual(TestSession.session1.login_peeradviser(2015103115, "hi"), True)
-
+    
     def test_wsncpw_login_peeradviser(self):
         self.assertEqual(TestSession.session1.login_peeradviser(2015103115, "lester"), True)
-
+    
     def test_csnwpw_login_peeradviser(self):
         self.assertEqual(TestSession.session1.login_peeradviser(2015103119, "password"), True)
-
+    
     def test_csncpw_login_peeradviser(self):
         self.assertEqual(TestSession.session1.login_peeradviser(2015103119, "lester"), True)
-
+    
     def test_logged_login_peeradviser(self):
         self.assertEqual(TestSession.session1.login_peeradviser(2015102105, "vince"), True)
         self.assertEqual(TestSession.session1.login_peeradviser(2015102105, "vince"), True)
     
-
 if __name__ == '__main__':
     unittest.main()
